101af750989e014585ec1ebb5c46dc74fdfebf4e
venkatSridarala <141898196+venkatSridarala@users.noreply.github.com>
2025-03-19 15:13:46 +0530

    Update ArrayProgram.java

--- ArrayProgram.java
+++ ArrayProgram.java
@@ -38,9 +38,84 @@ public class ArrayProgram{
 	}
 //	Output:
 //		Arr is less than arr1
+	public void duparr() {
+		int dup[]= {1,2,3,4,2,5,1};
+		System.out.println("The Duplicate numbers are:");
+		int j;
+		for(int i=0;i<dup.length;i++) {
+			for(j=i+1;j<dup.length;j++) {
+				if(dup[i]==dup[j]) {
+					System.out.println(dup[i]);
+					break;
+				}				
+			}
+		}
+	}
+//	Output:
+//		The Duplicate numbers are:
+//			1
+//			2
+	public void unique() {
+		int dup[]= {1,2,3,4,2,5,1};
+		int j;
+		System.out.println("The unique element are:");
+		for(int i=0;i<dup.length;i++) {
+			int count=0;
+			for(j=0;j<dup.length;j++) {
+				if(dup[i]==dup[j]) {
+					count++;
+				}
+			}
+			if(count==1) {
+				System.out.println(dup[i]);
+			}
+		}
+	}
+//	Output:
+//		The unique element are:
+//			3
+//			4
+//			5
+	public void leftshift() {
+		int arr[]= {1,2,3,4,5};
+		int first=arr[0];
+		int i;
+		System.out.println("The left shift elements are:");
+		for(i=0;i<arr.length-1;i++) {
+			arr[i]=arr[i+1];
+		}
+		arr[i]=first;
+		for(i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		
+	}
+//	Output:
+//		The left shift elements are:
+//			2 3 4 5 1 
+	public void rightshift() {
+		int arr[]= {1,2,3,4,5};
+		int last=arr[arr.length-1];
+		int i;
+		System.out.println("The right shift elements are:");
+		for(i=arr.length-1;i>0;i--) {
+			arr[i]=arr[i-1];
+		}
+		arr[i]=last;
+		for(i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
 		public static void main(String[] args) {	
 			ArrayProgram arr=new ArrayProgram();
 			arr.arrayprogram();
 			arr.b();
-		}
+			arr.duparr();
+			arr.unique();
+			arr.leftshift();
+			arr.rightshift();
+			
+	}
 }
